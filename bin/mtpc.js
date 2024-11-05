508739ca09f866f05c447f57e1c952b4a9316919
Wen Miao <freealong95@gmail.com>
2024-11-05 10:14:46 +0400

    feat: update bin

--- bin/mtpc.js
+++ bin/mtpc.js
@@ -1,22 +1,12 @@
 #!/usr/bin/env node
-const program = require('commander');
-const init = require('../lib/init.js');
-const load = require('../lib/load.js');
+const program = require("commander");
+const init = require("../lib/init.js");
+const load = require("../lib/load.js");
 
-program
-  .version(require('../package.json').version)
-  .command('init')
-  .description('创建新项目')
-  .action(init)
+program.version(require("../package.json").version);
 
-program
-  .version(require('../package.json').version)
-  .command("load")
-  .description("添加项目模板配置")
-  .option("-k, --key <key>", "模板键名")
-  .option("-n, --name <name>", "项目名称")
-  .option("-r, --repo <repo>", "项目仓库地址")
-  .option("-b, --branch <branch>", "项目分支", "main")
-  .action(load);
+program.command("init").description("创建新项目").action(init);
+
+program.command("load").description("添加项目模板配置").option("-k, --key <key>", "模板键名").option("-n, --name <name>", "项目名称").option("-r, --repo <repo>", "项目仓库地址").option("-b, --branch <branch>", "项目分支", "main").action(load);
 
 program.parse(process.argv);
